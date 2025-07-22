cb64bae79320f159556753590309e5eaa41cf755
daniel-m10 <daniel.montecinos.pinto@gmail.com>
2025-07-22 19:31:36 -0400

    add Application/DTOs, Application/Interfaces, Application/UseCases and CreateUserTests

---
+++ ToDoListApi.Application/DTOs/CreateUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoListApi.Application.DTOs;
+
+public class CreateUserRequest
+{
+    public string Email { get; init; } = null!;
+    public string Password { get; init; } = null!;
+}
+
---
+++ ToDoListApi.Tests/UseCases/Users/CreateUserTests.cs
@@ -0,0 +1,78 @@
+﻿using NSubstitute;
+using ToDoListApi.Application.DTOs;
+using ToDoListApi.Application.Interfaces;
+using ToDoListApi.Application.UseCases.Users;
+
+namespace ToDoListApi.Tests.UseCases.Users
+{
+    [TestFixture]
+    public class CreateUserTests
+    {
+        [Test]
+        public async Task CreateUser_ShouldReturnUserId_WhenDataIsValid()
+        {
+            // Arrange
+            var userRepository = Substitute.For<IUserRepository>();
+            var sut = new CreateUser(userRepository);   // System under test
+
+            var request = new CreateUserRequest
+            {
+                Email = "test@example.com",
+                Password = "Secure123!"
+            };
+
+            var expectedUserId = Guid.NewGuid();
+
+            userRepository
+                .CreateUserAsync(request.Email, request.Password)
+                .Returns(expectedUserId);
+
+            // Act
+            var result = await sut.CreateUserAsync(request);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedUserId));
+        }
+
+        [Test]
+        public void Create_User_ShouldThrowException_WhenEmailIsEmpty()
+        {
+            // Arrange
+            var userRepository = Substitute.For<IUserRepository>();
+            var sut = new CreateUser(userRepository);
+
+            var request = new CreateUserRequest
+            {
+                Email = string.Empty,
+                Password = "Password123!"
+            };
+
+            // Act & Assert
+            Assert.ThrowsAsync<ArgumentException>(async () =>
+            {
+                await sut.CreateUserAsync(request);
+            }, "Email cannot be empty.");
+        }
+
+        [Test]
+        public void CreateUser_ShouldThrowException_WhenPasswordIsEmpty()
+        {
+            // Arrange
+            var userRepository = Substitute.For<IUserRepository>();
+            var sut = new CreateUser(userRepository);
+
+            var request = new CreateUserRequest
+            {
+                Email = "test@example.com",
+                Password = "" // inválido
+            };
+
+            // Act & Assert
+            Assert.ThrowsAsync<ArgumentException>(async () =>
+            {
+                await sut.CreateUserAsync(request);
+            }, "Password cannot be empty.");
+        }
+
+    }
+}
