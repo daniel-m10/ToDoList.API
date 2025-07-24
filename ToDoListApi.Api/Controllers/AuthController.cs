0360f5e501d49586905820b6db073a60d979250c
Daniel Montecinos <daniel.montecinos.pinto@gmail.com>
2025-07-23 21:28:15 -0400

    [develop] Feature/add first rest endpoints (#8)
    * add Application/DTOs, Application/Interfaces, Application/UseCases and CreateUserTests
    
    * add login and register endpoints

---
+++ ToDoListApi.Api/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoListApi.Application.DTOs;
+using ToDoListApi.Application.Interfaces;
+
+namespace ToDoListApi.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController(IUserService userService, IAuthService authService) : ControllerBase
+    {
+        private readonly IUserService _userService = userService;
+        private readonly IAuthService _authService = authService;
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] CreateUserRequest request)
+        {
+            var userId = await _userService.CreateUserAsync(request);
+            return Ok(new { UserId = userId });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var userId = await _authService.LoginAsync(request);
+            return Ok(new { UserId = userId });
+        }
+    }
+}
--- ToDoListApi.Api/Program.cs
+++ ToDoListApi.Api/Program.cs
@@ -1,9 +1,19 @@
+using ToDoListApi.Application.Infrastructure;
+using ToDoListApi.Application.Interfaces;
+using ToDoListApi.Application.UseCases.Auth;
+using ToDoListApi.Application.UseCases.Users;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+builder.Services.AddSingleton<IPasswordHasher, Sha256PasswordHasher>();
+builder.Services.AddSingleton<IUserService, CreateUser>();
+builder.Services.AddSingleton<IAuthService, LoginUser>();
 
 var app = builder.Build();
 
@@ -15,30 +25,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.UseAuthorization();
+app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
